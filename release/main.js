d5ae940c21f746380541c224b3f6c9cc5303e11b
Wisang <wseom7@gmail.com>
2015-09-22 14:40:27 +0900

    grunt watch tested

--- release/main.js
+++ release/main.js
@@ -16,3 +16,4 @@ var userName = prompt('Hello, what\'s your name?');
 document.body.innerHTML = '<h1>Hello, ' + userName + '!</h1>' +
 	'<p>' + projectName + ' ' + versionNumber + 
 	' accessed on: ' + currentTime + '<p>';
+// this comment is to check grunt watch
