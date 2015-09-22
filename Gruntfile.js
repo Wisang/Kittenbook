fa2f35eb890334a43283570373fd0a459a36420e
Wisang <wseom7@gmail.com>
2015-09-22 14:31:11 +0900

    grunt watcher added

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+
+module.exports = function(grunt) {
+    grunt.initConfig({
+      concat: {
+        release: {
+          src: ['js/values.js', 'js/prompt.js'],
+          dest: 'release/main.js'
+        }
+      },
+      copy: {
+        release: {
+          src: 'manifest.json',
+          dest: 'release/manifest.json'
+        }
+      },
+      jshint: {
+        files: ['js/values.js', 'js/prompt.js']
+      }
+    });
+
+    // Load Grunt plugins
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+
+    // Register tasks
+    grunt.registerTask('default', ['jshint', 'concat', 'copy']);
+};
